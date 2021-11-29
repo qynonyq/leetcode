f7bb3c0f2f48675eba1e27815a399017c08d9f5a
Kirill Eliseev <kirill.eliseev@corp.mail.ru>
2021-11-29 18:21:13 +0300

    Improve code and test

--- easy/0001_two-sum.go
+++ easy/0001_two-sum.go
@@ -1,29 +1,16 @@
 package problems
 
-import "math"
-
 // https://leetcode.com/problems/two-sum/
 
+// TwoSum returns indexes of two elements which sum is equal to a target
 func TwoSum(nums []int, target int) []int {
 	result := make([]int, 0, 2)
-	max := int(math.Pow10(9))
-	min := -int(math.Pow10(9))
-	if len(nums) < 2 || len(nums) > 10000 {
-		return nil
-	}
-	if target < min || target > max {
-		return nil
-	}
-	for i := 1; i < len(nums); i++ {
-		if nums[i-1] > max || nums[i] > max {
-			return nil
-		}
-		if nums[i-1] < min || nums[i] < min {
-			return nil
-		}
-		if nums[i-1]+nums[i] == target {
-			result = []int{i - 1, i}
-			break
+	for i, n := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if n+nums[j] == target {
+				result = []int{i, j}
+				break
+			}
 		}
 	}
 	return result
--- easy/0001_two-sum_test.go
+++ easy/0001_two-sum_test.go
@@ -1,7 +1,6 @@
 package problems
 
 import (
-	"math"
 	"reflect"
 	"testing"
 )
@@ -14,62 +13,19 @@ type input struct {
 }
 
 func TestTwoSum(t *testing.T) {
-	bigAmount := make([]int, 10001, 10001)
-	for i := 1; i < len(bigAmount); {
-		bigAmount[i-1] = 2
-		bigAmount[i] = 3
-		i += 2
-	}
-	maxNum := int(math.Pow10(10))
-	minNum := -int(math.Pow10(10))
-	halfBillion := 5 * int(math.Pow10(8))
-	halfBillionNeg := -5 * int(math.Pow10(8))
-	inputs := []input{
-		{
-			desc:     "ok",
-			nums:     []int{2, 3},
-			target:   5,
-			expected: []int{0, 1},
-		}, {
-			desc:     "not found",
-			nums:     []int{2, 3},
-			target:   6,
-			expected: []int{},
-		}, {
-			desc:     "small amount",
-			nums:     []int{2},
-			target:   5,
-			expected: nil,
-		}, {
-			desc:     "big amount",
-			nums:     bigAmount,
-			target:   5,
-			expected: nil,
-		}, {
-			desc:     "max num",
-			nums:     []int{0, maxNum},
-			target:   5,
-			expected: nil,
-		}, {
-			desc:     "min num",
-			nums:     []int{0, minNum},
-			target:   5,
-			expected: nil,
-		}, {
-			desc:     "max target",
-			nums:     []int{halfBillion, halfBillion},
-			target:   maxNum,
-			expected: nil,
-		}, {
-			desc:     "min target",
-			nums:     []int{halfBillionNeg, halfBillionNeg},
-			target:   minNum,
-			expected: nil,
-		},
+	tests := map[string]struct {
+		nums     []int
+		target   int
+		expected []int
+	}{
+		"ok":        {nums: []int{2, 11, 7, 15}, target: 9, expected: []int{0, 2}},
+		"not found": {nums: []int{2, 11, 7, 15}, target: 5, expected: []int{}},
+		"one num":   {nums: []int{2}, target: 9, expected: []int{}},
+		"no num":    {nums: []int{}, target: 9, expected: []int{}},
 	}
 
-	for _, in := range inputs {
-		t.Run(in.desc, func(t *testing.T) {
+	for name, in := range tests {
+		t.Run(name, func(t *testing.T) {
 			result := TwoSum(in.nums, in.target)
 			if !reflect.DeepEqual(in.expected, result) {
 				t.Errorf("expected %#v, got %#v", in.expected, result)
